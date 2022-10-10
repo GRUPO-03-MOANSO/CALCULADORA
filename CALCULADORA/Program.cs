f2d45e7ff290befe6688b2712dfe7ee476b18208
Jose Santa Cruz <josestac@hotmail.com>
2022-10-09 23:12:13 -0500

    Agregando multiplicacion y division

--- CALCULADORA/Program.cs
+++ CALCULADORA/Program.cs
@@ -6,7 +6,7 @@ namespace CALCULADORA
         static void Main(string[] args)
         {
             //DECLARACION DE VARIABLES
-            int v1, v2, s, r;
+            int v1, v2, s, r, m, d;
             Console.WriteLine("INGRESE EL PRIMER NUMERO");
             v1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("INGRESE EL SEGUNDO NUMERO");
@@ -14,11 +14,13 @@ namespace CALCULADORA
             //REALIZAR OPERACIONES
             s = v1 + v2;
             r = v1 - v2;
+            m =  v1 * v2;
+            d = v1 / v2;
             //MOSTRAR RESULTADO
             Console.WriteLine($"LA OPERACION DE {v1} + {v2} ES :{s}");
             Console.WriteLine($"LA OPERACION DE {v1} - {v2} ES :{r}");
-
-
+            Console.WriteLine($"LA OPERACION DE {v1} * {v2} ES :{m}");
+            Console.WriteLine($"LA OPERACION DE {v1} / {v2} ES :{d}");
         }
     }
 }
